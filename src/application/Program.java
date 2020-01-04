4993ec4cf97ecc5307e32133cc96b46f4775f632
Lucas Tostes <lucastostes18@gmail.com>
2020-01-04 17:40:39 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,8 +11,6 @@ public class Program {
 
 		Department obj = new Department(1, "Books");
 
-		System.out.println(obj);
-
 		Seller seller = new Seller(21, "Lucas", "lucas1@gmail.com", new Date(), 3000.0, obj);
 
 		System.out.println(seller);
